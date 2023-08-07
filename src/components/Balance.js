52ddf82341959291cbb30eb70d08b2d5741a7f76
ajaykrishnan07 <ajkrish2002@gmail.com>
2023-08-08 00:20:49 +0530

    Added AppReducer
    Need to add math functions.

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {GlobalContext} from '../context/GlobalState'; 
 
 export const Balance = () => {
+
+  const {transactions} =  useContext(GlobalContext);
   return (
     <>
         <h4>Balance</h4>
